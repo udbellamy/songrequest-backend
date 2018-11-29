3ec7b6ce29b40546c65c7a89c133615b086d3987
dbellamy <dbellamy@184.52-14-84.ripe.coltfrance.com>
2018-11-29 14:36:16 +0100

    Debug

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,XMLHttpRequest');
   next();
 });
 
@@ -98,7 +98,7 @@ router.post("/postSongToQueue", (req, res) => {
   songToQueue.link=""
   songToQueue.save(err => {
     if (err) return res.json({ success: false, error: err });
-    return songToQueue.json({ success: true });
+    return res.json({ success: true });
   });
 });
 
