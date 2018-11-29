579f67ad09ae17e3779aff727919cb4f3dcf1c8f
dbellamy <dbellamy@184.52-14-84.ripe.coltfrance.com>
2018-11-29 10:16:27 +0100

    wrong command

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ router.get("/getSongList", (req, res) => {
 });
 
 router.get("/getQueue", (req, res) => {
-  let newQueue = new SongList({});
+  let newQueue = new Queue({});
       newQueue.save();
   Queue.find({}, {"artist": 1, "song": 1, "user": 1, "link": 1}, (err, Queue) => {
     if (err) return res.json({ success: false, error: err });
